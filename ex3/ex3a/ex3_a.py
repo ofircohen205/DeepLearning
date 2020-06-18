2048fffcf401510292d6053fe594fea627785b6e
Ofir Cohen <ofircohen2005@gmail.com>
2020-06-18 22:47:30 +0300

    update ex3b

--- ex3/ex3a/ex3_a.py
+++ ex3/ex3a/ex3_a.py
@@ -2,8 +2,10 @@
 # ID: 312255847
 # Date: 15/5/2020
 
-# from trains import Task
-# task = Task.init(project_name="Deep_Learning_Frameworks_Playground", task_name="babysitting_learning_process")
+from trains import Task
+task = Task.init(project_name="Babysetting_Learning_Process", task_name="babysitting_learning_process")
+logger = task.get_logger()
+
 import torch
 import torchvision
 import torchvision.transforms as transforms
@@ -20,21 +22,22 @@ from cnn_one import cnn_one_bn
 from cnn_two import cnn_two_bn
 from vgg_16 import vgg16_bn
 
-# from trains import Task
-# task = Task.init(project_name="Babysitting_learning_process", task_name="Babysitting_learning_process")
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-def fit_predict(model, epochs=2, lr=1e-3):
+
+def fit_predict(model, epochs=2, lr=1e-3, weight_decay=1e3):
     CUDA = torch.cuda.is_available()
     trainset, trainloader, testset, testloader, classes = load_cifar_10_data_tf()
     loss_function = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model['model'].parameters(), lr)
-    train_loss_values = []
-    accuracy_values = []
+    train_loss_values = {}
+    accuracy_values = {}
+    last_batch_value = None
     
     print("Starting training model: {}".format(model['name']))
+    iteration = 0
     for epoch in range(epochs):
         running_loss = 0.0
+        iteration += 1
         for i, data in enumerate(trainloader, 0):
             # Get images and labels
             images, labels = data
@@ -50,7 +53,7 @@ def fit_predict(model, epochs=2, lr=1e-3):
             running_loss += loss.item()
             if i %  2000 == 1999:
                 print("[{}, {}] loss: {}".format(epoch+1, i+1, running_loss/2000))
-                train_loss_values.append(running_loss / len(trainset))
+                train_loss_values[iteration] = running_loss / len(trainset)
                 running_loss = 0.0
     
     print("Finished training model: {}".format(model['name']))
@@ -59,16 +62,115 @@ def fit_predict(model, epochs=2, lr=1e-3):
     
     correct = 0
     total = 0
+    iteration = 0
     with torch.no_grad():
         for data in testloader:
+            iteration += 1
             images, labels = data
             outputs = model['model'](images)
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
-            accuracy_values.append(correct / total)
+            accuracy_values[iteration] = correct / total
     
     print("Accuracy of the network on the test images: {}".format(100 * correct / total))
+    # logger.report_line_plot(
+    #     title="Model {} loss".format(model['name']),
+    #     series=[{ 'name': 'Train', 'data': np.hstack((np.atleast_2d(train_loss_values.keys(), train_loss_values.values()))) }],
+    #     iteration=1,
+    #     xaxis="epoch",
+    #     yaxis="loss"
+    # )
+    # logger.report_line_plot(
+    #     title="Model {} accuracy".format(model['name']),
+    #     series=[{ 'name': 'Train', 'data': np.hstack((np.atleast_2d(accuracy_values.keys(), accuracy_values.values()))) }],
+    #     iteration=1,
+    #     xaxis="epoch",
+    #     yaxis="loss"
+    # )
+    plt.plot(train_loss_values)
+    plt.title("model {} loss".format(model['name']))
+    plt.ylabel("loss")
+    plt.xlabel("epoch")
+    plt.legend(['train'], loc='upper left')
+    plt.savefig('./plots/{}_loss_plot.png'.format(model['name']))
+    plt.clf()
+    plt.plot(accuracy_values)
+    plt.title("model {} accuracy".format(model['name']))
+    plt.ylabel("accuracy")
+    plt.xlabel("epoch")
+    plt.legend(['test'], loc='upper left')
+    plt.savefig('./plots/{}_accuracy_plot.png'.format(model['name']))
+    plt.clf()
+    print("====================================================")
+    
+''' End function '''
+
+
+def fit_predict_decay(model, epochs=2, lr=1e-3, weight_decay=1e3):
+    CUDA = torch.cuda.is_available()
+    trainset, trainloader, testset, testloader, classes = load_cifar_10_data_tf()
+    loss_function = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model['model'].parameters(), lr, weight_decay=weight_decay)
+    train_loss_values = {}
+    accuracy_values = {}
+    last_batch_value = None
+    
+    print("Starting training model: {} with weight decay".format(model['name']))
+    iteration = 0
+    for epoch in range(epochs):
+        running_loss = 0.0
+        iteration += 1
+        for i, data in enumerate(trainloader, 0):
+            # Get images and labels
+            images, labels = data
+            # zero gradient params
+            optimizer.zero_grad()
+            
+            # forward + backward + optimize
+            outputs = model['model'](images)
+            loss = loss_function(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            
+            running_loss += loss.item()
+            if i %  2000 == 1999:
+                print("[{}, {}] loss: {}".format(epoch+1, i+1, running_loss/2000))
+                train_loss_values[iteration] = running_loss / len(trainset)
+                running_loss = 0.0
+    
+    print("Finished training model: {} with weight decay".format(model['name']))
+    print("====================================================")
+    print("Starting testing model: {} with weight decay".format(model['name']))
+    
+    correct = 0
+    total = 0
+    iteration = 0
+    with torch.no_grad():
+        for data in testloader:
+            iteration += 1
+            images, labels = data
+            outputs = model['model'](images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            accuracy_values[iteration] = correct / total
+    
+    print("Accuracy of the network on the test images: {}".format(100 * correct / total))
+    # logger.report_line_plot(
+    #     title="Model {} loss".format(model['name']),
+    #     series=[{ 'name': 'Train', 'data': np.hstack((np.atleast_2d(train_loss_values.keys(), train_loss_values.values()))) }],
+    #     iteration=1,
+    #     xaxis="epoch",
+    #     yaxis="loss"
+    # )
+    # logger.report_line_plot(
+    #     title="Model {} accuracy".format(model['name']),
+    #     series=[{ 'name': 'Train', 'data': np.hstack((np.atleast_2d(accuracy_values.keys(), accuracy_values.values()))) }],
+    #     iteration=1,
+    #     xaxis="epoch",
+    #     yaxis="loss"
+    # )
     plt.plot(train_loss_values)
     plt.title("model {} loss".format(model['name']))
     plt.ylabel("loss")
@@ -125,6 +227,7 @@ def main():
     # Execute learning
     for model in nn_models:
         fit_predict(model)
+        fit_predict_decay(model)
 
 if __name__ == "__main__":
     main()
